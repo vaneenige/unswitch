61a48b51a1faa629652361fe464b27c10f5ef9cd
Matt Steele <orphum@gmail.com>
2018-08-03 20:24:41 -0500

    refactor variable names and extract methods

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
-const m = ['b', 'a', 'y', 'x', 'l', 'r'];
+const buttonMappings = ['b', 'a', 'y', 'x', 'l', 'r'];
 
-function Unswitch(s) {
-  this.a = 8;
-  this.b = {};
-  this.c = {};
-  for (let i = 0; i < m.length; i += 1) {
-    this.b[m[i]] = { p: false };
+function Unswitch(userSettings) {
+  this.axisPosition = 8;
+  this.buttonState = {};
+  this.settings = {};
+  for (let i = 0; i < buttonMappings.length; i += 1) {
+    this.buttonState[buttonMappings[i]] = { pressed: false };
   }
-  Object.assign(this.c, s);
+  Object.assign(this.settings, userSettings);
   this.update = () => {
     const gamepads = navigator.getGamepads();
     for (let i = 0; i < Object.keys(gamepads).length; i += 1) {
-      if (gamepads[i] && gamepads[i].id && gamepads[i].id.indexOf(this.c.side) !== -1) {
-        const pad = gamepads[i];
-        const { buttons, axes } = pad;
-        for (let j = 0; j < m.length; j += 1) {
-          const button = m[j];
-          if (this.b[button].p !== buttons[j].pressed && this.c[button]) {
-            this.b[button].p = buttons[j].pressed;
-            this.c[button](this.b[button].p);
-          }
-        }
-        if (this.c.axes) {
-          const position = Math.round(axes[9] / (2 / 7) + 3.5);
-          if (position !== this.a) {
-            this.c.axes(position);
-            this.a = position;
-          }
-        }
+      if (gamepads[i] && gamepads[i].id && gamepads[i].id.indexOf(this.settings.side) !== -1) {
+        this.updatePad(gamepads[i]);
         break;
+      }
+    }
+  };
+
+  this.updatePad = (pad) => {
+    const { buttons, axes } = pad;
+    for (let j = 0; j < buttonMappings.length; j += 1) {
+      const button = buttonMappings[j];
+      if (this.buttonState[button].pressed !== buttons[j].pressed && this.settings[button]) {
+        this.buttonState[button].pressed = buttons[j].pressed;
+        this.settings[button](this.buttonState[button].pressed);
+      }
+    }
+    if (this.settings.axes) {
+      const position = Math.round(axes[9] / (2 / 7) + 3.5);
+      if (position !== this.axisPosition) {
+        this.settings.axes(position);
+        this.axisPosition = position;
       }
     }
   };
