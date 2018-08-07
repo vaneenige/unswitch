59c7e9d7339f2a235ae0f499cf883a58f03a284f
Colin van Eenige <cvaneenige@gmail.com>
2018-08-07 19:00:01 +0200

    Update demo with all buttons.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,42 +1,39 @@
-import Controller from '../../src/index';
+import Unswitch from '../../src/index';
 
 const ul = document.querySelector('ul');
 
-function logButton(control, state) {
-  ul.innerHTML = `<li>${control} was ${state ? 'pressed' : 'released'}</li> ${ul.innerHTML}`;
+function lb(side, control, state) {
+  ul.innerHTML = `<li>${side} ${control} was ${state ? 'pressed' : 'released'}</li> ${ul.innerHTML}`;
 }
 
-function LogAxes(controller, position) {
+function la(controller, position) {
   ul.innerHTML = `<li>${controller} ~ Axes position ${position}</li> ${ul.innerHTML}`;
 }
 
-const controllerLeft = new Controller({
-  side: 'L',
-  b: pressed => logButton('1 ~ B', pressed),
-  a: pressed => logButton('1 ~ A', pressed),
-  y: pressed => logButton('1 ~ Y', pressed),
-  x: pressed => logButton('1 ~ X', pressed),
-  l: pressed => logButton('1 ~ L', pressed),
-  r: pressed => logButton('1 ~ R', pressed),
-  minus: pressed => logButton('1 ~ Minus', pressed),
-  screenshot: pressed => logButton('1 ~ Screenshot', pressed),
-  lstick: pressed => logButton('1 ~ L Stick', pressed),
-  axes: position => LogAxes('1', position),
-});
+function createController(s) {
+  const controller = new Unswitch({
+    side: s,
+    b: p => lb(s, ' ~ B', p),
+    a: p => lb(s, ' ~ A', p),
+    y: p => lb(s, ' ~ Y', p),
+    x: p => lb(s, ' ~ X', p),
+    sl: p => lb(s, ' ~ SL', p),
+    sr: p => lb(s, ' ~ SR', p),
+    minus: p => lb(s, ' ~ Minus ', p),
+    plus: p => lb(s, ' ~ Plus', p),
+    lstick: p => lb(s, ' ~ Lstick', p),
+    rstick: p => lb(s, ' ~ Rstick', p),
+    home: p => lb(s, ' ~ Home', p),
+    screenshot: p => lb(s, ' ~ Screenshot', p),
+    bumper: p => lb(s, ' ~ Bumper', p), // L or R
+    trigger: p => lb(s, ' ~ Trigger', p), // ZL or ZR
+    axes: p => la(s, p),
+  });
+  return controller;
+}
 
-const controllerRight = new Controller({
-  side: 'R',
-  b: pressed => logButton('2 ~ B', pressed),
-  a: pressed => logButton('2 ~ A', pressed),
-  y: pressed => logButton('2 ~ Y', pressed),
-  x: pressed => logButton('2 ~ X', pressed),
-  l: pressed => logButton('2 ~ L', pressed),
-  r: pressed => logButton('2 ~ R', pressed),
-  plus: pressed => logButton('2 ~ Plus', pressed),
-  home: pressed => logButton('2 ~ Home', pressed),
-  rstick: pressed => logButton('2 ~ R Stick', pressed),
-  axes: position => LogAxes('2', position),
-});
+const controllerLeft = createController('L');
+const controllerRight = createController('R');
 
 function render() {
   controllerLeft.update();
